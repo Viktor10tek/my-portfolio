fe8d4fe51f05a0b358bbd3ec3f0c21007cc35129
Viktor10tek <vicadams4real@gmail.com>
2026-09-19 04:27:22 +0100

    Allow Vercel frontend in CORS

--- server/server.ts
+++ server/server.ts
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-vercel-domain.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://victor-adama-portfolio.vercel.app",
+    ],
   }),
 );
 
